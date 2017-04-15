62414ff298c6dae8c2855d9dceff7138deac32ae
Fahad Qazi <fahadqazi@gmail.com>
2017-04-15 23:35:29 +0100

    adding a new component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AlbumList = () => {
+    return (
+        <View>
+            <Text>Dummy Data here</Text>
+        </View>
+    );
+};
+
+export default AlbumList;
