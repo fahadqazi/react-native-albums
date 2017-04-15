d7fa15170fcd77f5616146dfd489b72348faa874
Fahad Qazi <fahadqazi@gmail.com>
2017-04-15 23:37:20 +0100

    changing from functional component to class component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Dummy Data here</Text>
-        </View>
-    );
-};
+class AlbumList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Dummy Data here</Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList;
