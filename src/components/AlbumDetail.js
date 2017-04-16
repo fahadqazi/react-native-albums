bc79a483da5c2da3f5a59cce8a6d99ab581364a6
Fahad Qazi <fahadqazi@gmail.com>
2017-04-16 23:31:41 +0100

    display card done, added button and styling to each card

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,21 +2,34 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button.js';
 
 const AlbumDetail = ({ album }) => {
-    const { title, thumbnail_image, artist } = album;
+    const { title, thumbnail_image, artist, image } = album;
+    const { headerContentStyle, 
+        thumbnailStyle, 
+        thumbnailContainerStyle, 
+        headerTextStyle,
+        imageStyle } = styles;
 
     return (
         <Card>
             <CardSection>
-                <View>
-                    <Image style={styles.thumbnailStyle} srouce={{ uri: thumbnail_image }} />
+                <View style={thumbnailContainerStyle}>
+                    <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
                 </View>
-                
-                <View style={styles.headerContentStyle}>
-                    <Text>{title}</Text>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+ 
+            <CardSection>
+                <Image style={imageStyle} source={{ uri: image }} />
+            </CardSection>
+ 
+            <CardSection>
+                <Button />
             </CardSection>
         </Card>
     );
@@ -27,9 +40,23 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around',
     },
+    headerTextStyle: {
+        fontSize: 18,
+    },
     thumbnailStyle: {
         height: 50,
         width: 50,
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null,
     }
 };
 
