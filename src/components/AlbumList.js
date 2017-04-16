efd5da87fcfba69f2c40c6f971abc493ac368d44
Fahad Qazi <fahadqazi@gmail.com>
2017-04-16 11:17:26 +0100

    can now fetch data from api and display list to screen

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+    state = {
+        albums: []
+    }
+
     componentWillMount() {
-        
+        console.log('hi');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+        console.log(this.state);
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => <Text key={album.title}>{album.title}</Text>);
     }
 
     render() {
         return (
             <View>
-                <Text>Dummy Data here</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
